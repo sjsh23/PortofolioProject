8451817611dce8e1d46b5533b0907668be9e37fd
sjsh23 <satyasiddhijosh23@gmail.com>
2024-09-19 18:08:36 +0700

    push again

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,16 +4,16 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 function About() {
   return (
-    <section id="about" className="p-5">
+    <section id="about" className="p-5 d-flex justify-content-center align-items-center">
       <Container>
-        <h2>About Me</h2>
-        <Row>
-          <Col md={6}>
+        <h2 className="text-center mb-4">About Me</h2>
+        <Row className="justify-content-center align-items-center">
+          <Col md={6} className="text-center">
             <p>
-              I am a web and mobile developer with experience in building modern, responsive web applications using React, JavaScript, CSS frameworks, and Java using Android Studio.
+              I am a student in Bina Nusantara university majoring in computer science. I specialize in web and mobile developer with experience in building modern, responsive web applications using React, JavaScript, CSS frameworks, and Java using Android Studio.
             </p>
           </Col>
-          <Col md={6}>
+          <Col md={6} className="text-center">
             <img src="/profilepicture.jpg" alt="Profile" className="img-fluid rounded-circle" style={{ maxWidth: '200px', height: 'auto'}}  />
           </Col>
         </Row>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
     <section id="contact" className="p-5">
       <Container>
         <h2>Contact Me</h2>
-        <p>Email: [your-email@example.com]</p>
+        <p>Email: [satyasiddhi.josh@binus.ac.id]</p>
       </Container>
     </section>
   );
