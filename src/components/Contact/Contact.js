4e940a839e9a6368f29ba89763b143d78a1a221e
sjsh23 <satyasiddhijosh23@gmail.com>
2024-09-20 08:51:00 +0700

    contacts update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,45 @@
 // src/components/Contact.js
 import React from 'react';
 import '../styling.css';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Card, Col, Image } from 'react-bootstrap';
 
 function Contact() {
   return (
     <section id="contact" className="p-5 custom-heading">
       <Container>
-        <h2>Contact Me</h2>
-        <p>Email: [satyasiddhi.josh@binus.ac.id]</p>
+      <h2 className="text-center mb-4">Contact</h2>
+        <Row className="d-flex justify-content-center">
+          <Col md={6}>
+            <Card className="p-4 shadow-sm">
+              <Card.Body>
+                <Card.Text>
+                <Image 
+                    src="emailLogo.png" 
+                    alt="GitHub logo" 
+                    className="img-fluid me-2" 
+                    style={{ width: '24px'}} 
+                  />
+                  Email: satyasiddhi.josh@binus.ac.id</Card.Text>
+                
+
+                <Card.Text>
+
+                  <Image 
+                    src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" 
+                    alt="GitHub logo" 
+                    className="img-fluid me-2" 
+                    style={{ width: '24px'}} 
+                  />
+                  Github:
+                  <a href="https://github.com/sjsh23" target="_blank" rel="noopener noreferrer">
+                    sjsh23
+                  </a>
+                </Card.Text>
+                
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </section>
   );
