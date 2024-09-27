c44a0a399890b8627cf354f0026be30edf21edac
sjsh23 <satyasiddhijosh23@gmail.com>
2024-09-27 20:17:44 +0700

    add gradient

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap';
 
 function Home() {
   return (
-    <section id="home" className="p-5 bg-light text-center custom-heading">
+    <section id="home" className="p-5 bg-light text-center custom-heading bg-gradient">
       <Container>
         <h1>Hi, I'm [Satyasiddhi Josh]</h1>
         <p>Web Developer | Mobile Developer | Coder</p>
