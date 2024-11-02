9e623e599ce5a55f622dd6add6a1531ca30ea6cc
sjsh23 <satyasiddhijosh23@gmail.com>
2024-11-02 23:04:27 +0700

    test

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,7 @@
 // src/components/Projects.js
 import React from 'react';
 import '../styling.css';
-import { Container, Carousel, Button } from 'react-bootstrap';
+import { Container, Card, Col, Row, Carousel, Button } from 'react-bootstrap';
 
 function Projects() {
   return (
@@ -12,114 +12,128 @@ function Projects() {
         <Carousel className="project-carousel">
           {/* Finder Project */}
           <Carousel.Item>
-            <img
-              className="d-block w-100 carousel-img rounded shadow-lg"
-              src="logo512.png"
-              alt="Finder Project"
-            />
-            <Carousel.Caption className="project-caption">
-              <h3>Finder</h3>
-              <p>
-                Finder is a web-based app that helps university students and office workers find accommodations. It uses React.js.
-              </p>
-              <Button variant="light" href="https://github.com/sjsh23/Finder.-ASP.Net-.git" className="project-btn">View Project</Button>
-            </Carousel.Caption>
+            <Row className="d-flex justify-content-center">
+              <Col md={4}>
+                <Card className="project-card shadow-lg">
+                  <Card.Img variant="top" src="logo512.png" className="carousel-img" />
+                  <Card.Body>
+                    <Card.Title>Finder</Card.Title>
+                    <Card.Text>
+                      Finder is a web-based app that helps university students and office workers find accommodations. It uses React.js.
+                    </Card.Text>
+                    <Button variant="light" href="https://github.com/sjsh23/Finder.-ASP.Net-.git" className="project-btn">View Project</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Carousel.Item>
 
           {/* Galaxy Cinema Project */}
           <Carousel.Item>
-            <img
-              className="d-block w-100 carousel-img rounded shadow-lg"
-              src="nextJs.png"
-              alt="Galaxy Cinema Project"
-            />
-            <Carousel.Caption className="project-caption">
-              <h3>Galaxy Cinema</h3>
-              <p>
-                Galaxy Cinema is a web app for booking movie tickets. It uses Next.js and has a fully functional backend.
-              </p>
-              <Button variant="light" href="https://github.com/dhirojap/cinema" className="project-btn">View Project</Button>
-            </Carousel.Caption>
+            <Row className="d-flex justify-content-center">
+              <Col md={4}>
+                <Card className="project-card shadow-lg">
+                  <Card.Img variant="top" src="nextJs.png" className="carousel-img" />
+                  <Card.Body>
+                    <Card.Title>Galaxy Cinema</Card.Title>
+                    <Card.Text>
+                      Galaxy Cinema is a web app for booking movie tickets. It uses Next.js and has a fully functional backend.
+                    </Card.Text>
+                    <Button variant="light" href="https://github.com/dhirojap/cinema" className="project-btn">View Project</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Carousel.Item>
 
           {/* CalcWithSupport Project */}
           <Carousel.Item>
-            <img
-              className="d-block w-100 carousel-img rounded shadow-lg"
-              src="logo512.png"
-              alt="CalcWithSupport Project"
-            />
-            <Carousel.Caption className="project-caption">
-              <h3>CalcWithSupport</h3>
-              <p>
-                CalcWithSupport is a web-based calculator with a support page feature. It is built using React.js.
-              </p>
-              <Button variant="light" href="https://github.com/sjsh23/Calc3WithSupport" className="project-btn">View Project</Button>
-            </Carousel.Caption>
+            <Row className="d-flex justify-content-center">
+              <Col md={4}>
+                <Card className="project-card shadow-lg">
+                  <Card.Img variant="top" src="logo512.png" className="carousel-img" />
+                  <Card.Body>
+                    <Card.Title>CalcWithSupport</Card.Title>
+                    <Card.Text>
+                      CalcWithSupport is a web-based calculator with a support page feature. It is built using React.js.
+                    </Card.Text>
+                    <Button variant="light" href="https://github.com/sjsh23/Calc3WithSupport" className="project-btn">View Project</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Carousel.Item>
 
           {/* MakeMeUppzz Project */}
           <Carousel.Item>
-            <img
-              className="d-block w-100 carousel-img rounded shadow-lg"
-              src="pngegg.png"
-              alt="MakeMeUppzz Project"
-            />
-            <Carousel.Caption className="project-caption">
-              <h3>MakeMeUppzz</h3>
-              <p>
-                MakeMeUppzz is a C# ASP.Net web app using ado.net. It showcases models, entities, CRUD operations, and more.
-              </p>
-              <Button variant="light" href="https://github.com/sjsh23/MakeMeUpzz.git" className="project-btn">View Project</Button>
-            </Carousel.Caption>
+            <Row className="d-flex justify-content-center">
+              <Col md={4}>
+                <Card className="project-card shadow-lg">
+                  <Card.Img variant="top" src="pngegg.png" className="carousel-img" />
+                  <Card.Body>
+                    <Card.Title>MakeMeUppzz</Card.Title>
+                    <Card.Text>
+                      MakeMeUppzz is a C# ASP.Net web app using ado.net. It showcases models, entities, CRUD operations, and more.
+                    </Card.Text>
+                    <Button variant="light" href="https://github.com/sjsh23/MakeMeUpzz.git" className="project-btn">View Project</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Carousel.Item>
 
           {/* JCIWeb Project */}
           <Carousel.Item>
-            <img
-              className="d-block w-100 carousel-img rounded shadow-lg"
-              src="HTMLLogo.png"
-              alt="JCIWeb Project"
-            />
-            <Carousel.Caption className="project-caption">
-              <h3>JCIWeb</h3>
-              <p>
-                JCIWeb is a human-computer interaction project showcasing a convention center using HTML, CSS, and JavaScript.
-              </p>
-              <Button variant="light" href="https://github.com/sjsh23/JCIProject.git" className="project-btn">View Project</Button>
-            </Carousel.Caption>
+            <Row className="d-flex justify-content-center">
+              <Col md={4}>
+                <Card className="project-card shadow-lg">
+                  <Card.Img variant="top" src="HTMLLogo.png" className="carousel-img" />
+                  <Card.Body>
+                    <Card.Title>JCIWeb</Card.Title>
+                    <Card.Text>
+                      JCIWeb is a human-computer interaction project showcasing a convention center using HTML, CSS, and JavaScript.
+                    </Card.Text>
+                    <Button variant="light" href="https://github.com/sjsh23/JCIProject.git" className="project-btn">View Project</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Carousel.Item>
 
           {/* SimpleMobileCalc Project */}
           <Carousel.Item>
-            <img
-              className="d-block w-100 carousel-img rounded shadow-lg"
-              src="androidStudioLogo.png"
-              alt="SimpleMobileCalc Project"
-            />
-            <Carousel.Caption className="project-caption">
-              <h3>SimpleMobileCalc</h3>
-              <p>
-                SimpleMobileCalc is a simple calculator that runs on android OS using Java as the language, showcasing the basics of building a mobile app.
-              </p>
-              <Button variant="light" href="https://github.com/sjsh23/MobileCalcApp.git" className="project-btn">View Project</Button>
-            </Carousel.Caption>
+            <Row className="d-flex justify-content-center">
+              <Col md={4}>
+                <Card className="project-card shadow-lg">
+                  <Card.Img variant="top" src="androidStudioLogo.png" className="carousel-img" />
+                  <Card.Body>
+                    <Card.Title>SimpleMobileCalc</Card.Title>
+                    <Card.Text>
+                      SimpleMobileCalc is a simple calculator that runs on android OS using Java as the language, showcasing the basics of building a mobile app.
+                    </Card.Text>
+                    <Button variant="light" href="https://github.com/sjsh23/MobileCalcApp.git" className="project-btn">View Project</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Carousel.Item>
 
           {/* TaskManagerApp Project */}
           <Carousel.Item>
-            <img
-              className="d-block w-100 carousel-img rounded shadow-lg"
-              src="androidStudioLogo.png"
-              alt="TaskManagerApp Project"
-            />
-            <Carousel.Caption className="project-caption">
-              <h3>TaskManagerApp</h3>
-              <p>
-                TaskManagerApp is a Java mobile app that helps organize tasks by adding category, deadline, task description, and a simple search function.
-              </p>
-              <Button variant="light" href="https://github.com/sjsh23/TaskManagerApp.git" className="project-btn">View Project</Button>
-            </Carousel.Caption>
+            <Row className="d-flex justify-content-center">
+              <Col md={4}>
+                <Card className="project-card shadow-lg">
+                  <Card.Img variant="top" src="androidStudioLogo.png" className="carousel-img" />
+                  <Card.Body>
+                    <Card.Title>TaskManagerApp</Card.Title>
+                    <Card.Text>
+                      TaskManagerApp is a Java mobile app that helps organize tasks by adding category, deadline, task description, and a simple search function.
+                    </Card.Text>
+                    <Button variant="light" href="https://github.com/sjsh23/TaskManagerApp.git" className="project-btn">View Project</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Carousel.Item>
         </Carousel>
       </Container>
