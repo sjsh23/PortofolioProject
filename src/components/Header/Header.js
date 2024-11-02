b6b7e40548c6aa7020b559f8fb8a8b13c539f838
sjsh23 <satyasiddhijosh23@gmail.com>
2024-11-02 20:55:54 +0700

    fixing margin in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,56 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../styling.css';
 import { Navbar, Nav } from 'react-bootstrap';
 
-
 function Header() {
+  const [activeSection, setActiveSection] = useState('home');
+
+  // Function to handle setting the active section
+  const handleNavClick = (section) => {
+    setActiveSection(section);
+  };
+
   return (
     <Navbar bg="transparent" variant="light" expand="lg" sticky="top" className="custom-navbar p-3 shadow-sm">
       <Navbar.Brand href="#home" className="ms-3 fw-bold brand-name">
-      <img 
+        <img 
           src="sjLogo.png"
           alt="Logo" 
           className="navbar-logo" 
-          style={{ height: '40px' }} // Adjust height as needed
+          style={{ height: '40px' }}
         />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#home" className="nav-link-custom">Home</Nav.Link>
-          <Nav.Link href="#about" className="nav-link-custom">About</Nav.Link>
-          <Nav.Link href="#projects" className="nav-link-custom">Projects</Nav.Link>
-          <Nav.Link href="#contact" className="nav-link-custom">Contact</Nav.Link>
+          <Nav.Link 
+            href="#home"
+            onClick={() => handleNavClick('home')}
+            className={`nav-link-custom ${activeSection === 'home' ? 'active' : ''}`}
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link 
+            href="#about"
+            onClick={() => handleNavClick('about')}
+            className={`nav-link-custom ${activeSection === 'about' ? 'active' : ''}`}
+          >
+            About
+          </Nav.Link>
+          <Nav.Link 
+            href="#projects"
+            onClick={() => handleNavClick('projects')}
+            className={`nav-link-custom ${activeSection === 'projects' ? 'active' : ''}`}
+          >
+            Projects
+          </Nav.Link>
+          <Nav.Link 
+            href="#contact"
+            onClick={() => handleNavClick('contact')}
+            className={`nav-link-custom ${activeSection === 'contact' ? 'active' : ''}`}
+          >
+            Contact
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
